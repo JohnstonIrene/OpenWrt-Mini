dcc3d887aa10929c91e9f494807a197fa8a0369f
JohnstonIrene <154217523+JohnstonIrene@users.noreply.github.com>
2024-10-23 15:08:31 +0800

    Add files via upload

--- DIY-1.sh
+++ DIY-1.sh
@@ -4,7 +4,10 @@
 
 # 1-添加 ShadowSocksR Plus+ 插件
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> "feeds.conf.default"
+#临时使用 SSR+ 自用修改源
+# git clone https://github.com/JohnstonIrene/helloworld.git;master package/helloworld
+# echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> "feeds.conf.default"
+echo "src-git helloworld https://github.com/JohnstonIrene/helloworld.git;master" >> "feeds.conf.default"
 
 
 # 2-添加 PowerOff 关机插件
