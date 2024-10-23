9a0b5217930b9b8d0cba3eb065191a71c2a7514d
JohnstonIrene <154217523+JohnstonIrene@users.noreply.github.com>
2024-10-23 13:45:14 +0800

    Add files via upload

--- DIY-1.sh
+++ DIY-1.sh
@@ -2,8 +2,10 @@
 #=========================================================================================================================
 
 
-# 1-添加ShadowSocksR Plus+插件
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 1-添加 ShadowSocksR Plus+ 插件
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> "feeds.conf.default"
+
 
 # 2-添加 PowerOff 关机插件
 git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
@@ -16,4 +18,5 @@ sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.con
 
 # 5-添加PassWall插件
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
